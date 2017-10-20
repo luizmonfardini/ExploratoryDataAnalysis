e0b75493e467e70cdfb1aab33350c378c1955ee2
luizmonfardini <32209127+luizmonfardini@users.noreply.github.com>
2017-10-20 15:02:49 -0200

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -13,7 +13,7 @@ finalData <- plotData[plotData$Date %in% c("1/2/2007","2/2/2007"),]
 SetTime <-strptime(paste(finalData$Date, finalData$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
 finalData <- cbind(SetTime, finalData)
 ## Plot 1
-hist(finalData$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kw)")
+hist(finalData$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 ## Save file and close device
 dev.copy(png,"plot1.png", width=480, height=480)
 dev.off()
